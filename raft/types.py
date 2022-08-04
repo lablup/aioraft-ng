e3de9800ef327a7c83679f7f70371234732673f9
Jeongseok Kang <jskang@lablup.com>
2022-08-04 13:50:47 +0900

    setup: Apply `flake8`, `mypy`, `black`, `isort`

---
+++ raft/types.py
@@ -0,0 +1,42 @@
+from typing import Type, TypeVar
+
+PeerId = str
+
+T_aobj = TypeVar("T_aobj", bound="aobject")
+
+
+class aobject(object):
+    """
+    An "asynchronous" object which guarantees to invoke both ``def __init__(self, ...)`` and
+    ``async def __ainit(self)__`` to ensure asynchronous initialization of the object.
+
+    You can create an instance of subclasses of aboject in the following way:
+
+    .. code-block:: python
+
+       o = await SomeAObj.new(...)
+    """
+
+    @classmethod
+    async def new(cls: Type[T_aobj], *args, **kwargs) -> T_aobj:
+        """
+        We can do ``await SomeAObject(...)``, but this makes mypy
+        to complain about its return type with ``await`` statement.
+        This is a copy of ``__new__()`` to workaround it.
+        """
+        instance = super().__new__(cls)
+        cls.__init__(instance, *args, **kwargs)
+        await instance.__ainit__()
+        return instance
+
+    def __init__(self, *args, **kwargs) -> None:
+        pass
+
+    async def __ainit__(self) -> None:
+        """
+        Automatically called when creating the instance using
+        ``await SubclassOfAObject(...)``
+        where the arguments are passed to ``__init__()`` as in
+        the vanilla Python classes.
+        """
+        pass
