ca0e6fb58f7aff556b84f228bb2b8b759442e36c
Jeongseok Kang <jskang@lablup.com>
2022-08-29 17:03:59 +0900

    setup: Add version file

--- aioraft/__init__.py
+++ aioraft/__init__.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from .raft import Raft  # noqa: F401
 
-__version__ = "0.1.0-rc1"
+__version__ = (Path(__file__).parent / "VERSION").read_text().strip()
--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 import os
+from pathlib import Path
 
 from setuptools import setup
 
 ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
-VERSION = "0.1.0"
+VERSION = (Path(__file__).parent / "aioraft" / "VERSION").read_text().strip()
 description = (
     "An implementation of `Raft` consensus algorithm written in asyncio-based Python 3."
 )
@@ -29,8 +30,9 @@ setup(
     maintainer="rapsealk",
     maintainer_email="jskang@lablup.com",
     url="https://github.com/lablup/aioraft",
-    license="Apache-2.0",
+    license="Apache License 2.0",
     package_dir={"aioraft": "aioraft"},
+    package_data={"": ["VERSION"]},
     python_requires=">=3.10",
     install_requires=install_requires,
     extras_require={"dev": dev_requires},
