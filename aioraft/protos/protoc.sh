4440925c96eefb44af29d7720b5e7e15a6a5f9af
Jeongseok Kang <jskang@lablup.com>
2023-03-19 15:42:43 +0900

    deps: Bump grpcio to 1.51.3

--- aioraft/protos/protoc.sh
+++ aioraft/protos/protoc.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-python -m grpc_tools.protoc -I. --python_out=$2 --grpc_python_out=$2 $1
+python -m grpc_tools.protoc -I. --python_out=$2 --grpc_python_out=$2 --pyi_out=$2 $1
 sed -i "" '1s/^/# type: ignore\n/' $2/raft_pb2.py
 sed -i '' 's/import .*_pb2/from . import raft_pb2 as raft__pb2/' $2/raft_pb2_grpc.py
-protoc -I. --pyi_out=$2 $1
 python -m black $2/raft_pb2.py $2/raft_pb2_grpc.py $2/raft_pb2.pyi
 python -m isort $2/raft_pb2.py $2/raft_pb2.pyi
